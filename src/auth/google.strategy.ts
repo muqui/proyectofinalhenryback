8ef884fa50c29a91ce698655c0d50bdc28bb37f0
Alberto Corona <muqui@hotmail.com>
2025-02-02 23:45:59 -0600

    env

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -12,7 +12,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      //callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: process.env.CALLBACKURL,
       scope: ['profile', 'email'],
     });
   }
